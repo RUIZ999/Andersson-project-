732a3963f85ba4554d4cd0565b1a7a798cf66ca6
RUIZ999 <123671217+RUIZ999@users.noreply.github.com>
2023-01-26 11:35:24 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,15 @@
+let buttonOne= document.querySelector(".option-one");
+let openBoxDiv=document.querySelector(".option-one-screen");
+let buttonTwo=document.querySelector(".option-two");
+let runAwayDiv=document.querySelector(".option-two-screen");
+
+
+
+buttonOne.onclick=function(){
+   openBoxDiv.style.display="block";
+};
+
+buttonTwo.onclick=function(){
+    runAwayDiv.style.display="block";
+};
+
